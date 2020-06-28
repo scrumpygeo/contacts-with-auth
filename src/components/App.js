f8f6fb48e36b41adcdac180bf578c4e1d6e178dd
scrumpygeo <scrumpygeo@yahoo.ca>
2020-06-28 17:37:15 +0100

    add base routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import ContactCreate from '../components/contacts/ContactCreate';
+import ContactList from '../components/contacts/ContactList';
+import ContactShow from '../components/contacts/ContactShow';
+import ContactEdit from '../components/contacts/ContactEdit';
+import ContactDelete from '../components/contacts/ContactDelete';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route />
+          <Route path='/' exact component={ContactList} />
+          <Route path='/contacts/new' exact component={ContactCreate} />
+          <Route path='/contacts/edit' exact component={ContactEdit} />
+          <Route path='/contacts/delete' exact component={ContactDelete} />
+          <Route path='/contacts/show' exact component={ContactShow} />
         </div>
       </BrowserRouter>
     </div>
