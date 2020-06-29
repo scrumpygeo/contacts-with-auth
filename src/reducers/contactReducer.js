29dbda3140988bf3596bfd9dd573ccc1470350cc
scrumpygeo <scrumpygeo@yahoo.ca>
2020-06-29 16:01:23 +0100

    fetch create and edit contacts reducer

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -10,6 +10,10 @@ export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_CONTACT:
       return { ...state, [action.payload.id]: action.payload };
+    case CREATE_CONTACT:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_CONTACT:
+      return { ...state, [action.payload.id]: action.payload };
     default:
       return state;
   }
