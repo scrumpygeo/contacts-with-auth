c0ff3992b4efa7496bae73a288416caba044bcf7
scrumpygeo <scrumpygeo@yahoo.ca>
2020-06-29 15:32:17 +0100

    fetch contact reducer

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,16 @@
+import {
+  CREATE_CONTACT,
+  FETCH_CONTACTS,
+  FETCH_CONTACT,
+  EDIT_CONTACT,
+  DELETE_CONTACT,
+} from '../actions/types';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_CONTACT:
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
