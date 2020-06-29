7a4955be91822b285064e7305689e23e4f4b85ab
scrumpygeo <scrumpygeo@yahoo.ca>
2020-06-29 13:29:47 +0100

    write rest of action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,38 @@
 import contacts from '../apis/contacts';
+import {
+  CREATE_CONTACT,
+  FETCH_CONTACTS,
+  FETCH_CONTACT,
+  DELETE_CONTACT,
+  EDIT_CONTACT,
+} from './types';
 
 export const createContact = (formValues) => async (dispatch) => {
-  contacts.post('/contacts', formValues);
+  const response = await contacts.post('/contacts', formValues);
+
+  dispatch({ type: CREATE_CONTACT, payload: response.data });
+};
+
+export const fetchContacts = () => async (dispatch) => {
+  const response = await contacts.get('/contacts');
+
+  dispatch({ type: FETCH_CONTACTS, payload: response.data });
+};
+
+export const fetchContact = (id) => async (dispatch) => {
+  const response = await contacts.get(`/contacts/${id}`);
+
+  dispatch({ type: FETCH_CONTACT, payload: response.data });
+};
+
+export const editContact = (id, formValues) => async (dispatch) => {
+  const response = await contacts.put(`/contacts/${id}`, formValues);
+
+  dispatch({ type: EDIT_CONTACT, payload: response.data });
+};
+
+export const deleteContact = (id) => async (dispatch) => {
+  await contacts.delete(`/contacts/${id}`);
+
+  dispatch({ type: DELETE_CONTACT, payload: id });
 };
