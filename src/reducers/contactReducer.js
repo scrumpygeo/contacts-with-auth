9d599c90dcefc489bff79b26f079abebdbcdab20
scrumpygeo <scrumpygeo@yahoo.ca>
2020-06-29 17:03:43 +0100

    delete and list contacts reducers

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -9,6 +9,8 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_CONTACTS:
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
     case FETCH_CONTACT:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_CONTACT:
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import contactReducer from './contactReducer';
 
 export default combineReducers({
   form: formReducer,
+  contacts: contactReducer,
 });
