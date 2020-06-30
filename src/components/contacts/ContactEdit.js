bd461fdc3ff93f1726005aeb172e09e1fd6d3a66
scrumpygeo <scrumpygeo@yahoo.ca>
2020-06-30 14:46:50 +0100

    create ContactFrom and refactor redux form code to be generalized

--- src/components/contacts/ContactEdit.js
+++ src/components/contacts/ContactEdit.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchContact } from '../../actions';
 
-const Contactedit = () => {
-  return <div>Contact Edit</div>;
+class ContactEdit extends Component {
+  componentDidMount() {
+    this.props.fetchContact(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.contact) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.contact.email}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    contact: state.contacts[ownProps.match.params.id],
+  };
 };
 
-export default Contactedit;
+export default connect(mapStateToProps, { fetchContact })(ContactEdit);
---
+++ src/components/contacts/ContactForm.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+const email = (value) =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? 'Invalid email address'
+    : undefined;
+
+class ContactForm extends Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return <div className='text-danger'>{error}</div>;
+    }
+  }
+  renderInput = ({ input, label, meta }) => {
+    const className = `form-control ${
+      meta.error && meta.touched ? 'is-invalid' : ''
+    }`;
+    return (
+      <div className='form-group'>
+        <label>{label}</label>
+        <input {...input} className={className} autoComplete='off' />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues);
+  };
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field
+          name='first_name'
+          component={this.renderInput}
+          label='First Name'
+        />
+        <Field
+          name='last_name'
+          component={this.renderInput}
+          label='Last Name'
+        />
+        <Field
+          name='email'
+          component={this.renderInput}
+          label='Email'
+          validate={email}
+        />
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.first_name) {
+    errors.first_name = 'You must enter a first name.';
+  }
+
+  if (!formValues.last_name) {
+    errors.last_name = 'You must enter a last name.';
+  }
+
+  if (!formValues.email) {
+    errors.email = 'You must enter an email.';
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: 'streamForm',
+  validate,
+})(ContactForm);
