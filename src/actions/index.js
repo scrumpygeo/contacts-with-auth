eed7ad388adcb7f2a3c19a95d800b8792975f2d0
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-07-02 13:07:16 +0100

    change for different api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,41 +1,42 @@
-import contacts from '../apis/contacts';
-import history from '../history';
+import contacts from "../apis/contacts";
+import history from "../history";
 import {
   CREATE_CONTACT,
   FETCH_CONTACTS,
   FETCH_CONTACT,
   DELETE_CONTACT,
   EDIT_CONTACT,
-} from './types';
+} from "./types";
 
 export const createContact = (formValues) => async (dispatch) => {
-  const response = await contacts.post('/contacts', formValues);
+  const response = await contacts.post("/contacts", formValues);
 
-  dispatch({ type: CREATE_CONTACT, payload: response.data });
-  history.push('/');
+  dispatch({ type: CREATE_CONTACT, payload: response.data.data });
+  history.push("/");
 };
 
 export const fetchContacts = () => async (dispatch) => {
-  const response = await contacts.get('/contacts');
-  dispatch({ type: FETCH_CONTACTS, payload: response.data });
+  const response = await contacts.get("/contacts");
+  console.log(response);
+  dispatch({ type: FETCH_CONTACTS, payload: response.data.data });
 };
 
 export const fetchContact = (id) => async (dispatch) => {
   const response = await contacts.get(`/contacts/${id}`);
 
-  dispatch({ type: FETCH_CONTACT, payload: response.data });
+  dispatch({ type: FETCH_CONTACT, payload: response.data.data });
 };
 
 export const editContact = (id, formValues) => async (dispatch) => {
   const response = await contacts.patch(`/contacts/${id}`, formValues);
 
-  dispatch({ type: EDIT_CONTACT, payload: response.data });
-  history.push('/');
+  dispatch({ type: EDIT_CONTACT, payload: response.data.data });
+  history.push("/");
 };
 
 export const deleteContact = (id) => async (dispatch) => {
   await contacts.delete(`/contacts/${id}`);
 
   dispatch({ type: DELETE_CONTACT, payload: id });
-  history.push('/');
+  history.push("/");
 };
