b0532fda55d15c609575117d102a50950939e0a4
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-07-11 15:18:05 +0100

    change default settin on state

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -11,7 +11,7 @@ import {
 const initialState = {
   authentication_token: localStorage.getItem("authentication_token"),
   email: localStorage.getItem("email"),
-  isAuthenticated: true,
+  isAuthenticated: null,
   loading: false,
 };
 
